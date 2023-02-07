1c30019d385c780cc5075161b89aba4293db538c
adi1 <aditya93arpita96@gmail.com>
2023-02-07 23:46:48 +0100

    add pays method in main

--- main.py
+++ main.py
@@ -15,18 +15,21 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        return bill.amount / 2
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        return weight * bill.amount
 
 class Pdfreport:
     def __init__(self, filename):
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
 
-the_bill = Bill(amount=100, period="january 2022")
+
+the_bill = Bill(amount=120, period="january 2022")
 adi = Flatmate(name="adi", days_in_house = 20)
+arpita = Flatmate(name="arpita", days_in_house=25)
+print("Adi has to pay: ", adi.pays(bill= the_bill, flatmate2 = arpita))
+print("Arpita has to pay: ", arpita.pays(bill= the_bill, flatmate2 = adi))
 
-print(adi.pays(bill= the_bill))
 
